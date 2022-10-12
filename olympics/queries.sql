a3853145e7a9c0d7a4d1c1a207e6f9c658e76422
czhang2884 <zhangc3@carleton.edu>
2022-10-11 23:44:52 -0500

    finalized

--- olympics/queries.sql
+++ olympics/queries.sql
@@ -1,16 +1,3 @@
-/*
-List all the NOCs (National Olympic Committees), in alphabetical order by abbreviation. 
-    These entities, by the way, are mostly equivalent to countries. 
-    But in some cases, you might find that a portion of a country participated in a particular games 
-    (e.g. one guy from Newfoundland in 1904) or some other oddball situation.
-
-List the names of all the athletes from Jamaica. If your database design allows it, sort the athletes by last name.
-
-List all the medals won by Greg Louganis, sorted by year. Include whatever fields in this output that you think appropriate.
-
-List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
-*/
-
 '''
     queries.sql
     Carl Zhang, 12 Oct 2022
@@ -35,7 +22,3 @@ ORDER BY games_traits.year
 SELECT medal_counts.noc_name, medal_counts.gold
 FROM medal_counts
 ORDER BY medal_counts.gold
-
-/*
-t
-*/

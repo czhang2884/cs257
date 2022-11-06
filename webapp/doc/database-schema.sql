acfa462d6d9fe7507fe8e1ce43fc3f429c17298d
Alex Falk <falka@LAPTOP-SRKIJHQR.localdomain>
2022-11-06 11:36:27 -0600

    finished api-design.txt and database-schema.sql

--- webapp/doc/database-schema.sql
+++ webapp/doc/database-schema.sql
@@ -1,6 +1,34 @@
 CREATE TABLE movies {
-    id serial
-    movie_title text
-    movie_year float
-    
+    id serial,
+    movie_title text,
+    release_year float,
+    genre_id integer,
+    overview text,
+    popularity integer,
+    image_id integer,
+    review_id integer
+};
+
+CREATE TABLE reviews {
+    id serial,
+    movie_id integer,
+    review_score integer,
+    review_comment text,
+    users_name text
+};
+
+CREATE TABLE average_reviews {
+    movie_id integer,
+    average_review float
 }
+
+CREATE TABLE genres {
+    id serial,
+    genre text
+};
+
+CREATE TABLE images {
+    id serial,
+    movie_id integer,
+    image_link text
+};

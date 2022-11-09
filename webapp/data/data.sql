3b2ef222d53011ad60aa1f2295753555e4a1c81b
Alex Falk <falka@LAPTOP-SRKIJHQR.localdomain>
2022-11-09 13:33:54 -0600

    Fixed api.py to match with .css

--- webapp/data/data.sql
+++ webapp/data/data.sql
@@ -16,6 +16,71 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: movies; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.movies (
+    id integer NOT NULL,
+    movie_title text,
+    release_year double precision,
+    genre_id integer,
+    overview text,
+    popularity integer,
+    image_id integer,
+    review_id integer
+);
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.movies_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.movies_id_seq OWNED BY public.movies.id;
+
+
+--
+-- Name: movies id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.movies ALTER COLUMN id SET DEFAULT nextval('public.movies_id_seq'::regclass);
+
+
+--
+-- Data for Name: movies; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY public.movies (id, movie_title, release_year, genre_id, overview, popularity, image_id, review_id) FROM stdin;
+1	Inside Out	2015	5	Great movie	100	3	5
+2	The Room	2003	2	Terrible but wonderful	2	5	2
+3	How to Train Your Dragon	2010	5		60	1	4
+\.
+
+
+--
+-- Name: movies_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('public.movies_id_seq', 1, false);
+
+
 --
 -- PostgreSQL database dump complete
 --

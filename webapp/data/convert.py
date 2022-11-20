c0bb0b2a15889a04b0046d7e35c9bf00d40bf36c
CS Lab Account <cslab@olin310-17.mathcs.carleton.edu>
2022-11-20 13:04:08 -0600

    small changes to convert.py

--- webapp/data/convert.py
+++ webapp/data/convert.py
@@ -22,177 +22,177 @@ import os
 import json
 import re
 
-# # CREATE TABLE movies (
-# #     id numeric,
-# #     movie_title text,
-# #     release_year integer,
-# #     genre text,
-# #     overview text,
-# #     popularity numeric,
-# #     director_id text,
-# #     mubi_url text,
-# #     title_lang text,
-# #     orig_lang text,
-# #     runtime integer,
-# #     adult integer
-# # );
+# CREATE TABLE movies (
+#     id numeric,
+#     movie_title text,
+#     release_year integer,
+#     genre text,
+#     overview text,
+#     popularity numeric,
+#     director_id text,
+#     mubi_url text,
+#     title_lang text,
+#     orig_lang text,
+#     runtime integer,
+#     adult integer
+# );
 
-# movie = {}
-# with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
-#         open('movies.csv', 'w') as movies_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(movies_file)
-#     heading_row = next(reader)
-#     for row in reader:
-#         movie_id = row[1]
-#         title = row[2]
-#         release_year = row[3]
-#         genres = row[14]
-#         overiew = row[19]
-#         popularity = row[6]
-#         director_id = row[8]
-#         mubi_url = row[4]
-#         title_lang = row[5]
-#         orig_lang = row[18]
-#         runtime = row[26]
-#         if row[11]:
-#             adult = 1
-#         elif not row[11]:
-#             adult = 0
+movie = {}
+with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
+        open('movies.csv', 'w') as movies_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(movies_file)
+    heading_row = next(reader)
+    for row in reader:
+        movie_id = row[1]
+        title = row[2]
+        release_year = row[3]
+        genres = row[14]
+        overiew = row[19]
+        popularity = row[6]
+        director_id = row[8]
+        mubi_url = row[4]
+        title_lang = row[5]
+        orig_lang = row[18]
+        runtime = row[26]
+        if row[11]:
+            adult = 1
+        elif not row[11]:
+            adult = 0
 
-#         if movie_id not in movie:
-#             movie[movie_id] = title
-#             writer.writerow([movie_id, title, release_year, genres, overiew, popularity, director_id, mubi_url, title_lang, orig_lang, runtime, adult])
-# print("done with movies convert")
+        if movie_id not in movie:
+            movie[movie_id] = title
+            writer.writerow([movie_id, title, release_year, genres, overiew, popularity, director_id, mubi_url, title_lang, orig_lang, runtime, adult])
+print("done with movies convert")
 
-# # CREATE TABLE users (
-# #     user_id integer,
-# #     profile_pic text
-# # );
+# CREATE TABLE users (
+#     user_id integer,
+#     profile_pic text
+# );
 
-# users = {}
-# with open('../doc/datasets/mubi_ratings_user_data.csv') as original_data_file,\
-#         open('users.csv', 'w') as users_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(users_file)
-#     heading_row = next(reader)
-#     for row in reader:
-#         user_id = row[1]
-#         pic = row[5]
+users = {}
+with open('../doc/datasets/mubi_ratings_user_data.csv') as original_data_file,\
+        open('users.csv', 'w') as users_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(users_file)
+    heading_row = next(reader)
+    for row in reader:
+        user_id = row[1]
+        pic = row[5]
         
-#         if user_id not in users:
-#             users[user_id] = 1
-#             writer.writerow([user_id, pic])
-# print("done with users convert")
+        if user_id not in users:
+            users[user_id] = 1
+            writer.writerow([user_id, pic])
+print("done with users convert")
 
-# # CREATE TABLE profit (
-# #     movie_id integer,
-# #     budget numeric,
-# #     revenue numeric
-# # );
+# CREATE TABLE profit (
+#     movie_id integer,
+#     budget numeric,
+#     revenue numeric
+# );
 
-# profit = {}
-# with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
-#         open('profit.csv', 'w') as profit_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(profit_file)
-#     heading_row = next(reader)
-#     for row in reader:
-#         movie_id = row[1]
-#         budget = row[13]
-#         revenue = row[25]
+profit = {}
+with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
+        open('profit.csv', 'w') as profit_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(profit_file)
+    heading_row = next(reader)
+    for row in reader:
+        movie_id = row[1]
+        budget = row[13]
+        revenue = row[25]
 
-#         if movie_id not in profit:
-#             profit[movie_id] = 1
-#             writer.writerow([movie_id, budget, revenue])
-# print("done with profit convert")
+        if movie_id not in profit:
+            profit[movie_id] = 1
+            writer.writerow([movie_id, budget, revenue])
+print("done with profit convert")
 
-# # CREATE TABLE voting_average (
-# #     movie_id integer,
-# #     vote_avg float,
-# #     vote_count integer
-# # );
+# CREATE TABLE voting_average (
+#     movie_id integer,
+#     vote_avg float,
+#     vote_count integer
+# );
 
-# voting_average = {}
-# with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
-#         open('voting_average.csv', 'w') as voting_average_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(voting_average_file)
-#     heading_row = next(reader)
-#     for row in reader:
-#         movie_id = row[1]
-#         vote_avg = row[32]
-#         vote_count = row[33]
+voting_average = {}
+with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
+        open('voting_average.csv', 'w') as voting_average_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(voting_average_file)
+    heading_row = next(reader)
+    for row in reader:
+        movie_id = row[1]
+        vote_avg = row[32]
+        vote_count = row[33]
 
-#         if movie_id not in voting_average:
-#             voting_average[movie_id] = 1
-#             writer.writerow([movie_id, vote_avg, vote_count])
-# print("done with voting_average convert")
+        if movie_id not in voting_average:
+            voting_average[movie_id] = 1
+            writer.writerow([movie_id, vote_avg, vote_count])
+print("done with voting_average convert")
 
-# # CREATE TABLE genres (
-# #     id serial,
-# #     genre text
-# # );
+# CREATE TABLE genres (
+#     id serial,
+#     genre text
+# );
 
-# genres = {}
-# with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
-#         open('genres.csv', 'w') as genres_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(genres_file)
-#     heading_row = next(reader)
-#     for row in reader:
-#         genre = eval(row[14])
-#         for dict in genre:
-#             if dict['id'] not in genres:
-#                 genres[dict['id']] = dict['name']
-#     for key in genres:
-#         writer.writerow([key, genres[key]])
-# print("done with genres convert")
+genres = {}
+with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
+        open('genres.csv', 'w') as genres_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(genres_file)
+    heading_row = next(reader)
+    for row in reader:
+        genre = eval(row[14])
+        for dict in genre:
+            if dict['id'] not in genres:
+                genres[dict['id']] = dict['name']
+    for key in genres:
+        writer.writerow([key, genres[key]])
+print("done with genres convert")
 
-# # CREATE TABLE images (
-# #     movie_id integer,
-# #     image_link text
-# # );
+# CREATE TABLE images (
+#     movie_id integer,
+#     image_link text
+# );
 
-# images = {}
-# with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
-#         open('images.csv', 'w') as images_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(images_file)
-#     heading_row = next(reader)
-#     for row in reader:
-#         movie_id = row[1]
-#         url = row[7]
+images = {}
+with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
+        open('images.csv', 'w') as images_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(images_file)
+    heading_row = next(reader)
+    for row in reader:
+        movie_id = row[1]
+        url = row[7]
 
-#         if movie_id not in images:
-#             images[movie_id] = 1
-#             writer.writerow([movie_id, url])
-# print("done with images convert")
+        if movie_id not in images:
+            images[movie_id] = 1
+            writer.writerow([movie_id, url])
+print("done with images convert")
 
-# # CREATE TABLE directors (
-# #     id serial,
-# #     name text,
-# #     director_url text
-# # );
+# CREATE TABLE directors (
+#     id serial,
+#     name text,
+#     director_url text
+# );
 
-# directors = {}
-# with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
-#         open('directors.csv', 'w') as directors_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(directors_file)
-#     heading_row = next(reader)
-#     for row in reader:
-#         director_id = row[8].split(", ")
-#         name = row[9].split(", ")
-#         url = row[10].split(", ")
+directors = {}
+with open('../doc/datasets/movies_joined_info.csv') as original_data_file,\
+        open('directors.csv', 'w') as directors_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(directors_file)
+    heading_row = next(reader)
+    for row in reader:
+        director_id = row[8].split(", ")
+        name = row[9].split(", ")
+        url = row[10].split(", ")
 
-#         for i in range(0,len(director_id)):
-#             if director_id[i] not in directors:
-#                 directors[director_id[i]] = [name[i], url[i]]
+        for i in range(0,len(director_id)):
+            if director_id[i] not in directors:
+                directors[director_id[i]] = [name[i], url[i]]
 
-#     for key in directors:
-#         writer.writerow([key, directors[key][0], directors[key][1]])
-# print("done with directors convert")
+    for key in directors:
+        writer.writerow([key, directors[key][0], directors[key][1]])
+print("done with directors convert")
 
 # CREATE TABLE reviews (
 #     rating_id integer,
@@ -231,30 +231,30 @@ print("done with reviews convert")
 #     num_reviews integer
 # );
 
-# avg_ratings = {}
-# with open('../doc/datasets/mubi_ratings_data.csv') as original_data_file,\
-#         open('average_reviews', 'w') as average_reviews_file:
-#     reader = csv.reader(original_data_file)
-#     writer = csv.writer(average_reviews_file)
-#     heading_row = next(reader)
-#     count = 1
-#     for row in reader:
-#         count += 1
-#         movie_id = row[0]
-#         score = row[3]
-#         if score != '':
-#             score = int(float(row[3]))
-#             if movie_id not in avg_ratings:
-#                 avg_ratings[movie_id] = [0,0]
-#                 avg_ratings[movie_id][0] += 1
-#                 avg_ratings[movie_id][1] = avg_ratings[movie_id][1] + score
-#             else:
-#                 avg_ratings[movie_id][0] += 1
-#                 avg_ratings[movie_id][1] += score
+avg_ratings = {}
+with open('../doc/datasets/mubi_ratings_data.csv') as original_data_file,\
+        open('average_reviews', 'w') as average_reviews_file:
+    reader = csv.reader(original_data_file)
+    writer = csv.writer(average_reviews_file)
+    heading_row = next(reader)
+    count = 1
+    for row in reader:
+        count += 1
+        movie_id = row[0]
+        score = row[3]
+        if score != '':
+            score = int(float(row[3]))
+            if movie_id not in avg_ratings:
+                avg_ratings[movie_id] = [0,0]
+                avg_ratings[movie_id][0] += 1
+                avg_ratings[movie_id][1] = avg_ratings[movie_id][1] + score
+            else:
+                avg_ratings[movie_id][0] += 1
+                avg_ratings[movie_id][1] += score
 
-#     for key in avg_ratings:
-#         writer.writerow([key, avg_ratings[key][1]/avg_ratings[key][0],avg_ratings[key][0]])
-# print("done with average_reviews convert")
+    for key in avg_ratings:
+        writer.writerow([key, avg_ratings[key][1]/avg_ratings[key][0],avg_ratings[key][0]])
+print("done with average_reviews convert")
 
 ########### OLDER VERSION ###########
 
